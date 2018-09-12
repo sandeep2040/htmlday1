9484f8840e799a35f689a77474160525192959a2
sandeep2040 <chandasai121@gmail.com>
2018-09-12 18:09:09 +0530

    hello

---
+++ task6.js
@@ -0,0 +1,10 @@
+<html>
+
+<body></body>
+    
+First number:<input type="text" id="num1">
+second number:<input type="text" id="num2">
+<input type="button" onclick="add()" value="addition">
+    <p id="hello">
+    
+        
